8908b40f6aa0e0c18fb1f883028405446eb4d44f
Renan Silva <renanrambuls@gmail.com>
2021-05-13 10:33:54 -0300

    components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+    state = {
+      name: 'Otávio Miranda',
+      counter: 0
+    };
+  
+
+  handlePClick = () => {
+    this.setState({ name: 'Júnior'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
   render(){
+    const { name, counter } = this.state.name;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}> 
+          {name} {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Este é o link
         </a>
       </header>
     </div>
