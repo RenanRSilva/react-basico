884820e82c726ac1455de76fa40152df53d248ec
Renan Silva <renanrambuls@gmail.com>
2021-05-16 08:31:49 -0300

    buttton e organização

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,12 +3,16 @@ import { Component } from 'react';
 import './styles.css';
 
 
-import { loadPosts } from './utils/load-posts';
-import { Posts } from './Posts';
+import { loadPosts } from '../..//utils/load-posts';
+import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
 
-class App extends Component {
+class Home extends Component {
   state = {
-    posts: []
+    allPosts: [],
+    posts: [],
+    page: 0,
+    postsPerPage: 53
   };
 
   async componentDidMount() {
@@ -16,16 +20,46 @@ class App extends Component {
   }
 
   loadPosts = async () => {
-    const postsAndPhotos = await loadPosts
-    this.setState({ posts: postsAndPhotos });
+    const { page, postsPerPage } = this.state;
+
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
         <Posts posts={posts} />
+
+
+        <div className="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
+
       </section>
 
     );
@@ -33,4 +67,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default Home;
